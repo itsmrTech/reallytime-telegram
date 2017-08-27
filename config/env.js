6a33503dc099d461e48ae14750e13f9a1701758c
mrtech <Shamot.group@gmail.com>
2017-08-27 12:35:57 +0430

    mongodb address

--- config/env.js
+++ config/env.js
@@ -1,3 +1,4 @@
 module.exports={token:'441996257:AAFI6BoISHlkQXw4YICXmr9AGk-BU-wjtPA',
-db:'178.33.79.204:27017/reallytime'}
-// module.exports={token:'399005653:AAHCJnysvGT5HIgj1rTfjp6UqDCGvR4Qybc'}
+db:'mongodb://admin:admin@ds159963.mlab.com:59963/reallytime'}
+// module.exports={token:'399005653:AAHCJnysvGT5HIgj1rTfjp6UqDCGvR4Qybc',
+// db:'localhost:27017/reallytime'}
